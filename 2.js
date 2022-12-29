e32d356c62b1fd4bf1d6a7d481a1f35af17f48ff
Amoon5 <121638788+Amoon5@users.noreply.github.com>
2022-12-29 23:03:23 +0100

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,30 @@
+function myOverFunction(){
+
+    var top1 = Math.floor(Math.random() * 500);
+    var left1 = Math.floor(Math.random() * 1000); 
+    var top2 =top1 + "px";
+    var left2 =left1 + "px";
+    document.getElementById("logo").style.top= top2;
+    document.getElementById("logo").style.left= left2;
+}
+
+
+
+
+
+
+/* var i, j, k ;
+for(i=1; k<=5; i++) 
+{
+for(k=1; k<=(5-i); k++)
+{
+document.write("&nbsp");
+}
+for(j=1; j<=i; j++)
+{
+document.write("*");
+}
+document.write("<br/>");
+} */
+
+
